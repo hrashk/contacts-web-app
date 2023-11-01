b65d43c26cb1151b339f0017796d3ae8b6c39ca3
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2023-11-01 10:15:26 +0100

    initial commit

---
+++ src/test/java/io/github/hrashk/contacts/web/app/ContactsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hrashk.contacts.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
